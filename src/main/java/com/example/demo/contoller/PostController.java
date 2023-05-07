8621d388163c2133d1d22a410489992e0f5d772d
ijeongtaeg <falloutvoy@gmail.com>
2023-05-08 00:42:24 +0900

    add PostController.java

---
+++ src/main/java/com/example/demo/contoller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.demo.contoller;
+
+import com.example.demo.dto.PersonDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+    @PostMapping(value = "/default")
+    public String postMethod() {
+        return "Hello Post API!";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody PersonDTO personDTO) {
+        return personDTO.toString();
+    }
+}
